21beb6ae989bccf04a526a16e277f7edbc449903
ZachMuzzle <zach12342@me.com>
2025-02-18 20:29:47 +0000

    Finished first reactJS web app

---
+++ README.md
@@ -0,0 +1,3 @@
+# First ReactJS web app
+
+This is my first reactJS webapp it follows a tutorial provide by [Next.js](https://nextjs.org/learn/react-foundations). It goes over how to build a reactJs app natively followed by installing nextJS to better your workflow.
---
+++ app/like-button.jsx
@@ -0,0 +1,14 @@
+/* Backend server side */
+'use client';
+
+import { useState } from 'react';
+
+export default function LikeButton() {
+    const [likes, setLikes] = useState(0);
+    
+    function handleClick() {
+        setLikes(likes + 1);
+      }
+
+    return <button onClick={handleClick}>Like ({likes})</button>
+}
